3a131e8b8d7a248bf015205d2cff1064c4a98546
ProfessorParadox <43827667+ProfessorParadox@users.noreply.github.com>
2022-02-23 19:46:05 +0530

    added changes

---
+++ udemy-timbuch/src/mod9/pt2/ex1/Button.java
@@ -0,0 +1,29 @@
+package mod9.pt2.ex1;
+
+//inner class nested intro
+
+public class Button {
+    private String title;
+    private OnClickListener onClickLister;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickLister = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickLister.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+
+}
